6d64883fc92ec3e62e121423345272699d42e9f8
Niall <Niall.cullinane3@mail.dcu.ie>
2020-06-15 21:47:08 +0100

    added functions to make d_ need to test

--- src/Laminart.jl
+++ src/Laminart.jl
@@ -36,6 +36,99 @@ end
 # todo use saved static(?) of [u+] and [u-]???
 
 # LGN
+function fun_v(v::AbstractArray, u::AbstractArray, lgn_A::AbstractArray, lgn_B::AbstractArray, δ_v)
+    return δ_v .* ( -v +
+            ((1 - v) * relu(u) * (1 + lgn_A)) -
+            ((1 + v) * lgn_B))
+    end
+
+
+
+
+
+
+# lgn to l6 and l4
+
+#
+
+function fun_v_C(v_p::AbstractArray, v_m::AbstractArray, σ::Real, K::Int, γ=10, l = 4*ceil(Int,σ)+1)
+    # isodd(l) || throw(ArgumentError("length must be odd"))
+
+# todo replace gaussian kernel with static
+    V = exp(-1/8) .* (imfilter((relu.(v_p)-relu.(v_m)), Kernel.gaussian(σ), "circular"))
+
+    A = reshape(Array{eltype(V)}(undef, size(V)[1], size(V)[2]*K),size(V)[1],size(V)[2],K)
+    B = copy(A)
+
+# todo replace kern_A() and kern_B with premade kernels here
+    for k in 1:K
+        θ = π*(k-1)/K
+        A[:,:,k] = imfilter(V, LamKernels.kern_A(σ, θ), "circular")
+        B[:,:,k] = abs.(imfilter(V, LamKernels.kern_B(σ, θ), "circular"))
+    end
+
+    return γ .* (relu.(A .- B) .+ relu.(.- A .- B))
+end
+
+
+# L6
+function fun_dx_V1(x::AbstractArray, C::AbstractArray, z::AbstractArray, x_v2::AbstractArray, α, ϕ, Γ, δ_c, V_21, att=0)
+    return δ_c .* (-x .+
+            ((1 .- x) .*
+                ((α*C) .+ (ϕ .* relu.(z .- Γ)) .+ (V_21 .* x_v2) .+ att)))
+    end
+
+
+#     L4 excit
+function fun_dy(y::AbstractArray, C::AbstractArray, x::AbstractArray, m::AbstractArray, η_p, kern_W_p, μ, ν, n, δ_c)
+    return  δ_c(   -y .+
+            ((1 .- y) .* (C .+ (η_p .* x))) .-
+            ((1 .+ y) .* fun_f((m .* imfilter(m, kern_W_p), μ, ν, n))))
+    end
+
+
+#     l4 inhib
+function fun_dm(m::AbstractArray, x::AbstractArray, kern_W_m, η_m, μ, ν, n, δ_m)
+    return δ_m .* (  -m .+
+                     (η_m .* x) -
+                     (m .* fun_f.(imfilter(m, kern_W_m), μ, ν, n)))
+    end
+
+
+#     L2/3 excit
+function fun_dz(z::AbstractArray, y::AbstractArray, T_p, H, λ, Γ, ψ, δ_z, a_ex_23, att)
+    return δ_z .*   (-z .+
+                    ((1 .- z) .*
+                        ((λ .* relu.(y)) .+ imfilter((relu.(z .- Γ)), H) .+ (a_ex_23 .* att))) .-
+                    ((z .+ ψ) .* (imfilter(s, T_p))))
+    end
+
+
+#     L2/3 inhib
+function fun_ds(s::AbstractArray, z::AbstractArray, H, a_23_in, T_m, δ_s, att)
+    return δ_s .*   ( -s .+
+                    imfilter((relu.(z .- Γ)), H) .+ (a_23_in .* att) .-
+                    (s .* imfilter(s, T_m)))  #?????
+    end
+
+
+#     V2 L6
+function fun_dx_v2(x_v2::AbstractArray, z_v2::AbstractArray, z::AbstractArray, v_12_6, Γ, ϕ, δ_c, att)
+    return δ_c .*   (  -x_v2 .+
+                    ((1 .- x_v2) .*
+                        ((v_12_6 .* relu.(z .- Γ)) + (ϕ .* relu.(z_v2 .- Γ)) .+ att)))
+    end
+
+
+# V2 L4 excit
+function fun_dy_v2(y_v2::AbstractArray, z::AbstractArray, x_v2::AbstractArray, m_v2::AbstractArray, W_p, v_12_4, Γ, η_p, μ, ν, n, δ_c)
+    return δ_c .*   (-y_v2 .+
+                    ((1 .- y_v2) .* ((v_12_4 .* relu.(z .- Γ)) .+ (η_p .* x_v2))) .-
+                    ((1 .+ y_v2) .* fun_f.(imfilter(m_v2, W_p), μ, ν, n)))
+end
+
+
+
 
 # dv_p = δ_v( -v_p +
 #             ((1 - v_p) * relu(u) * (1 + lgn_A)) -
@@ -51,47 +144,6 @@ end
 # lgn_A = C_1 * imfilter(x, kern_sumk(K))
 #
 # lgn_B = C_2 * imfilter(x, Kernel.gaussian(σ_1), "circular")
-
-
-# lgn to l6 and l4
-
-#
-
-function fun_v_C(v_p::AbstractArray, v_m::AbstractArray, σ::Real, K::Int, γ=10, l = 4*ceil(Int,σ)+1)
-    # isodd(l) || throw(ArgumentError("length must be odd"))
-
-    V = exp(-1/8) .* (imfilter((relu.(v_p)-relu.(v_m)), Kernel.gaussian(σ), "circular"))
-
-    A = reshape(Array{eltype(V)}(undef, size(V)[1], size(V)[2]*K),size(V)[1],size(V)[2],K)
-    B = copy(A)
-
-# todo replace kern_A() and kern_B with premade kernels here
-    for k in 1:K
-        θ = π*(k-1)/K
-        A[:,:,k] = imfilter(V, LamKernels.kern_A(σ, θ), "circular")
-        B[:,:,k] = abs.(imfilter(V, LamKernels.kern_B(σ, θ), "circular"))
-    end
-
-    return γ .* (relu.(A .- B) .+ relu.(.- A .- B))
-end
-
-function fun_v_C(v_p::AbstractArray, v_m::AbstractArray, σ::Real, K::Int, γ=10, l = 4*ceil(Int,σ)+1)
-    # isodd(l) || throw(ArgumentError("length must be odd"))
-
-    V = exp(-1/8) .* (imfilter((relu.(v_p)-relu.(v_m)), Kernel.gaussian(σ), "circular"))
-
-    A = reshape(Array{eltype(V)}(undef, size(V)[1], size(V)[2]*K),size(V)[1],size(V)[2],K)
-    B = copy(A)
-
-# todo replace kern_A() and kern_B with premade kernels here
-    for k in 1:K
-        θ = π*(k-1)/K
-        A[:,:,k] = imfilter(V, LamKernels.kern_A(σ, θ), "circular")
-        B[:,:,k] = abs.(imfilter(V, LamKernels.kern_B(σ, θ), "circular"))
-    end
-
-    return γ .* (relu.(A .- B) .+ relu.(.- A .- B))
-end
 
 #
 # # L6
@@ -208,12 +260,12 @@ end
 
 # l4 excit
 function fun_y_equ(C::AbstractArray, x::AbstractArray,  m::AbstractArray, W_p, η_p)
-    return fun_equ.(C .+ (η_p .* x - fun_f.(imfilter(m, W_p))))
+    return fun_equ.(C .+ (η_p .* x - fun_f.(imfilter(m, W_p), μ, ν, n)))
 end
 
 # l4 inhib, needs initial condition of itself
-function fun_m_equ(C::AbstractArray, x::AbstractArray, m_init::AbstractArray, W_p, η_p)
-    return (η_m .* x ./ (1 .+  fun_f.(imfilter(m_init, W_p))))
+function fun_m_equ(C::AbstractArray, x::AbstractArray, m_init::AbstractArray, W_p, η_p, μ, ν, n)
+    return (η_m .* x ./ (1 .+  fun_f.(imfilter(m_init, W_p), μ, ν, n)))
 end
 
 
@@ -258,9 +310,9 @@ end
 
 
 #  V2 L4 excit
-function fun_yV2_equ(z::AbstractArray, x::AbstractArray,  m::AbstractArray, W_p, η_p, Γ, V_12_4, att=0)
-    return (V_12_4 .* max.(z,Γ) .+ (η_p .* x) - fun_f.(imfilter(m, W_p)) ./
-   (1 .+ V_12_4 .* max.(z,Γ) .+ (η_p .* x) .+ fun_f.(imfilter(m, W_p))
+function fun_yV2_equ(z::AbstractArray, x::AbstractArray,  m::AbstractArray, W_p, η_p, Γ, μ, ν, n, V_12_4, att=0)
+    return (V_12_4 .* max.(z,Γ) .+ (η_p .* x) - fun_f.(imfilter(m, W_p), μ, ν, n) ./
+   (1 .+ V_12_4 .* max.(z,Γ) .+ (η_p .* x) .+ fun_f.(imfilter(m, W_p), μ, ν, n)
 end
 
 end
