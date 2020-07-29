bd6a50433fbaa28182a11999b00cd0ec22512110
Niall Cullinane <niall.cullinane3@mail.dcu.ie>
2020-07-29 11:54:33 +0100

    dependables updated

--- src/MEngProject.jl
+++ src/MEngProject.jl
@@ -1,12 +1,13 @@
 module MEngProject
 include("./Laminart.jl")
 include("./LaminartGPU.jl")
+include("./LaminartConv.jl")
 include("./Laminartv1.jl")
 include("./LamKernels.jl")
 include("./Utils.jl")
 include("./Parameters.jl")
 using ModelingToolkit, OrdinaryDiffEq, CUDA
-export Laminart, LaminartGPU, LamKernels, Laminartv1, Utils, Parameters, testa, kern_A
+export Laminart, LaminartGPU, LaminartConv, LamKernels, Laminartv1, Utils, Parameters, testa, kern_A
 
 
 end
