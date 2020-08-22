4a753a3f7dac1ded7988ba91c413ae300d40428c
Niall Cullinane <niall.cullinane3@mail.dcu.ie>
2020-08-22 20:16:16 +0100

    removed old plots

--- scripts/.ipynb_checkpoints/bench_dim-checkpoint.jl
+++ scripts/.ipynb_checkpoints/bench_dim-checkpoint.jl
@@ -1,120 +1,297 @@
 using DrWatson
 @quickactivate "MEngProject"
-using MEngProject, CUDA, DifferentialEquations, PyPlot, NNlib,  ImageFiltering, Images, MEngProject.LaminartKernels, MEngProject.LaminartInitFunc, MEngProject.Utils, BenchmarkTools, Test
+using MEngProject,
+    CUDA,
+    DifferentialEquations,
+    PyPlot,
+    NNlib,
+    ImageFiltering,
+    Images,
+    MEngProject.LaminartKernels,
+    MEngProject.LaminartInitFunc,
+    MEngProject.Utils,
+    BenchmarkTools,
+    Test
 
-using OrdinaryDiffEq, ParameterizedFunctions, LSODA, Sundials, DiffEqDevTools, Noise
+using OrdinaryDiffEq,
+    ParameterizedFunctions, LSODA, Sundials, DiffEqDevTools, Noise
 
 batch = 1
 
 
-files = readdir(datadir("img"))
+# files = readdir(datadir("res_test"))[2:end]
+files = ["ver_lines_025_gs.png",
+ "ver_lines_050_gs.png",
+ "ver_lines_075_gs.png",
+ "ver_lines_100_gs.png",
+ "ver_lines_200_gs.png",
+ "ver_lines_300_gs.png",
+ "ver_lines_400_gs.png",]
 
-@inbounds begin
-	tspan = (0.0f0,800f0)
-
-	batch_ = string(batch,"_",rand(1000:9999))
-	mkdir(plotsdir(string("batch",batch_)))
-	for file in files[2:end]
-
-		p = LaminartInitFunc.parameterInit_conv_gpu(datadir("img",file), Parameters.parameters_f32);
-
-		u0 = cu(reshape(zeros(Float32, p.dim_i, p.dim_j*(5*p.K+2)), p.dim_i, p.dim_j, 5*p.K+2,1))
-
-		arr1 = similar(u0[:, :, 1:2,:])
-		arr2 = similar(u0[:, :, 1:1,:])
-
-		f = LaminartFunc.LamFunction(
-			similar(arr1), #x
-			similar(arr1), #m
-			similar(arr1), #s
-			similar(arr2), #x_lgn,
-			similar(arr1), #C,
-			similar(arr1), #H_z,
-			similar(arr1), # dy_temp,
-			similar(arr1), # dm_temp,
-			similar(arr1), # dz_temp,
-			similar(arr1), # ds_temp,
-			similar(arr2), # dv_temp,
-			similar(arr1), # H_z_temp,
-			similar(arr2), #  V_temp_1,
-			similar(arr2), #  V_temp_2,
-			similar(arr1), #  A_temp,
-			similar(arr1), #   B_temp
-		)
-
-		prob = ODEProblem(f, u0, tspan, p)
-	# 	@benchmark sol = solve(prob)
-		sol = solve(prob)
+global benchm_gpu = []
+global benchm_cpu = []
+global y1Res_gpu = []
+global y1Res_cpu = []
 
 
-		for t ∈ [25,50,100,200,400,800]
-# 				for t ∈ [25,50,100]
+    tspan = (0.0f0, 10f0)
 
-			v0 = @view sol(t)[:,:,:,1]
-			axMax = findmax(v0)[1]
-
-			for k ∈ 1:2:10
-				fig, ax = plt.subplots()
-				
-				v1 = @view sol(t)[:,:,k,1]
-				v2 = @view sol(t)[:,:,k+1,1]
-				im = ax.imshow(v1, cmap=matplotlib.cm.PRGn,
-							   vmax=axMax, vmin=-axMax)
-				im2 = ax.imshow(v2, cmap=matplotlib.cm.RdBu_r,
-							   vmax=axMax, vmin=-axMax, alpha=0.5)
-
-				cbar = fig.colorbar(im2,  shrink=0.9, ax=ax)
-				cbar.ax.set_xlabel("\$k=$k2\$")
-						cbar = fig.colorbar(im,  shrink=0.9, ax=ax)
-				cbar.ax.set_xlabel("\$k=$k\$")
-				layer=Utils.layers[k]
-					plt.title("Layer: $layer, \$t=$t\$")
-					plt.axis("off")
-					fig.tight_layout()
-				plt.savefig(plotsdir(string("batch",batch_),string(file,"_",t,"_",Utils.la[k],".png")))
-				close("all")
-			end
+    batch_ = string(batch, "_", rand(1000:9999))
+    mkdir(plotsdir(string("bench_dim", batch_)))
 
 
-			k=11
-			fig, ax = plt.subplots()
-			v1 = @view sol[:,:,k,1,t]
-			v2 = @view sol[:,:,k+1,1,t]
-			im = ax.imshow(v1, cmap=matplotlib.cm.PRGn,
-						   vmax=axMax, vmin=-axMax)
-			im2 = ax.imshow(v2, cmap=matplotlib.cm.RdBu_r,
-						   vmax=axMax, vmin=-axMax, alpha=0.5)
+    test_name = ["025", "050", "075", "100", "200", "300", "400"]
+    test_name_plt = [
+        "\$25×25\$",
+        "\$50×50\$",
+        "\$75×75\$",
+        "\$100×100\$",
+        "\$200×200\$",
+        "\$300×300\$",
+        "\$400×400\$",
+    ]
 
-			cbar = fig.colorbar(im2,  shrink=0.9, ax=ax)
-			cbar.ax.set_xlabel("\$v^-\$")
-					cbar = fig.colorbar(im,  shrink=0.9, ax=ax)
-			cbar.ax.set_xlabel("\$v^+\$")
 
-			layer=Utils.layers[k]
-				plt.title("Layer: $layer, \$t=$t\$")
-				plt.axis("off")
-				fig.tight_layout()
+    for file in enumerate(files[1:1])
+		try
+        p = LaminartInitFunc.parameterInit_conv_gpu(
+            datadir("res_test", file[2]),
+            Parameters.parameters_f32,
+        )
 
-			plt.savefig(plotsdir(string("batch",batch_),string(file,"_",t,"_",Utils.la[k],".png")))
-			close("all")
+        u0 = cu(reshape(
+            zeros(Float32, p.dim_i, p.dim_j * (5 * p.K + 2)),
+            p.dim_i,
+            p.dim_j,
+            5 * p.K + 2,
+            1,
+        ))
+
+        arr1 = similar(u0[:, :, 1:2, :])
+        arr2 = similar(u0[:, :, 1:1, :])
+
+        f = LaminartFunc.LamFunction(
+            arr1, #x
+            similar(arr1), #m
+            similar(arr1), #s
+            arr2, #x_lgn,
+            similar(arr1), #C,
+            similar(arr1), #H_z,
+            similar(arr1), # dy_temp,
+            similar(arr1), # dm_temp,
+            similar(arr1), # dz_temp,
+            similar(arr1), # ds_temp,
+            similar(arr2), # dv_temp,
+            similar(arr1), # H_z_temp,
+            similar(arr2), #  V_temp_1,
+            similar(arr2), #  V_temp_2,
+            similar(arr1), #  A_temp,
+            similar(arr1), #   B_temp
+        )
+
+        prob = ODEProblem(f, u0, tspan, p)
+        push!(benchm_gpu, @benchmark solve(prob))
+        sol = solve(prob)
+
+		@inbounds begin
+        t = 10
+        v0 = @view sol(t)[:, :, :, 1]
+        axMax = findmax(v0)[1]
+
+
+        k = 7
+		k2 = 8
+        fig, ax = plt.subplots()
+
+        v1 = @view sol(t)[:, :, k, 1]
+        v2 = @view sol(t)[:, :, k+1, 1]
+        im = ax.imshow(
+            v1,
+            cmap = matplotlib.cm.PRGn,
+            vmax = axMax,
+            vmin = -axMax,
+        )
+        im2 = ax.imshow(
+            v2,
+            cmap = matplotlib.cm.RdBu_r,
+            vmax = axMax,
+            vmin = -axMax,
+            alpha = 0.5,
+        )
+
+        cbar = fig.colorbar(im2, shrink = 0.9, ax = ax)
+        cbar.ax.set_xlabel("\$k=$k2\$")
+        cbar = fig.colorbar(im, shrink = 0.9, ax = ax)
+        cbar.ax.set_xlabel("\$k=$k\$")
+        layer = Utils.layers[k]
+        plt.title(string(
+            "Layer: $layer, \$t=$t\$, resolution=",
+            test_name_plt[file[1]],
+        ))
+        plt.axis("off")
+        fig.tight_layout()
+        plt.savefig(plotsdir(
+            string("bench_dim", batch_),
+            string(
+                file[2],
+                "_res_",
+                test_name[file[1]],
+                "_t",
+                t,
+                "_",
+                Utils.la[k],
+                ".png",
+            ),
+        ))
+        close("all")
+
+		v3 = @view sol[:,:,7:7,:,:]
+		push!(y1Res, Array(v3))
+		u0 = nothing
+		p = nothing
+		arr1 = nothing
+		arr2 = nothing
+		f = nothing
+		prob = nothing
+		sol = nothing
 		end
-
-
-	# time plot
-		fig, axs = plt.subplots()
-
-		for k ∈ 1:12
-			v3 = @view sol[:,:,k,1,end]
-			v4 = @view sol[findmax(v3)[2][1],findmax(v3)[2][2],k,1,:]
-			layer=Utils.layers_1[k]
-			axs.plot(v4,Utils.lines[k], label="$layer")
-		end
-		axs.set_xlabel("Time")
-		axs.set_ylabel("Activation")
-		plt.legend()
-		fig.tight_layout()
-		plt.savefig(plotsdir(string("batch",batch_),string(file,"_time.png")))
-		close("all")
 	end
-	
+		
+		try
+
+		p = LaminartInitFunc.parameterInit_conv_cpu(
+            datadir("res_test", file[2]),
+            Parameters.parameters_f32,
+        )
+
+        u0 = reshape(
+            zeros(Float32, p.dim_i, p.dim_j * (5 * p.K + 2)),
+            p.dim_i,
+            p.dim_j,
+            5 * p.K + 2,
+            1,
+        )
+
+        arr1 = similar(u0[:, :, 1:2, :])
+        arr2 = similar(u0[:, :, 1:1, :])
+
+        f = LaminartFunc.LamFunction(
+            arr1, #x
+            similar(arr1), #m
+            similar(arr1), #s
+            arr2, #x_lgn,
+            similar(arr1), #C,
+            similar(arr1), #H_z,
+            similar(arr1), # dy_temp,
+            similar(arr1), # dm_temp,
+            similar(arr1), # dz_temp,
+            similar(arr1), # ds_temp,
+            similar(arr2), # dv_temp,
+            similar(arr1), # H_z_temp,
+            similar(arr2), #  V_temp_1,
+            similar(arr2), #  V_temp_2,
+            similar(arr1), #  A_temp,
+            similar(arr1), #   B_temp
+        )
+
+        prob = ODEProblem(f, u0, tspan, p)
+        push!(benchm_cpu, @benchmark solve(prob))
+        sol = solve(prob)
+
+		push!(y1Res_cpu, sol[:,:,7:7,:,:])
+		u0 = nothing
+		p = nothing
+		arr1 = nothing
+		arr2 = nothing
+		f = nothing
+		prob = nothing
+		sol = nothing
+		end
+	end
+
+    # time plot
+    fig, axs = plt.subplots()
+
+    for result ∈ enumerate(y1Res_gpu)
+		lab = "$test_name[result[1]]"
+        axs.plot(result[2][findmax(result[2][:, :, 1, 1, end])[2][1], findmax(result[2][:, :, 1, 1, end])[2][2], k, 1, :], c = Utils.Colour[result[1]], "--", label = "$lab GPU")
+    end
+
+	for result ∈ enumerate(y1Res_cpu)
+		lab = "$test_name[result[1]]"
+		axs.plot(result[2][findmax(result[2][:, :, 1, 1, end])[2][1], findmax(result[2][:, :, 1, 1, end])[2][2], k, 1, :], c = Utils.Colour[result[1]],":", label = "$lab CPU")
+	end
+    axs.set_xlabel("Time")
+    axs.set_ylabel("Activation")
+    plt.title("L2/3, \$k=1\$")
+    plt.legend()
+    fig.tight_layout()
+    plt.savefig(plotsdir(
+        string("bench_dim", batch_),
+        string(file[2], "_para_", test_name[file[1]], "_time.png"),
+    ))
+    close("all")
+
+
+
+    # benchmark plot
+
+    fig, ax = plt.subplots()
+    for bm ∈ enumerate(benchm_gpu)
+        ax.scatter(
+		median(bm[2].times) * 1e-9,
+		test_name_plt[bm[1]],
+            label = "GPU",
+			color=Utils.colours[1],
+            alpha = 0.3,
+            edgecolors = "none",
+        )
+    end
+
+	for bm ∈ enumerate(benchm_cpu)
+		ax.scatter(
+            median(bm[2].times) * 1e-9,
+			test_name_plt[bm[1]],
+			label = "CPU",
+			alpha = 0.3,
+			color=Utils.colours[2],
+			edgecolors = "none",
+		)
+	end
+
+    ax.legend()
+	axs.set_xlabel("Resolution (\$px\$)")
+    axs.set_ylabel("Time (\$s\$)")
+    ax.grid(True)
+    fig.tight_layout()
+    plt.savefig(plotsdir(
+        string("bench_dim", batch_),
+        string(file[2], "_para_", test_name[file[1]], "_time.png"),
+    ))
+    close("all")
+
+	# memory
+	fig, ax = plt.subplots()
+    for bm ∈ enumerate(benchm_gpu)
+        ax.scatter(
+            bm[2].memory * 1e-6,
+            test_name_plt[bm[1]],
+			olor=Utils.colours[1],
+            label = "GPU",
+            alpha = 0.3,
+            edgecolors = "none",
+        )
+    end
+
+    ax.legend()
+	axs.set_xlabel("Resolution (\$px\$)")
+    axs.set_ylabel("Memory")
+    ax.grid(True)
+    fig.tight_layout()
+    plt.savefig(plotsdir(
+        string("bench_dim", batch_),
+        string(file[2], "_para_", test_name[file[1]], "_time.png"),
+    ))
+    close("all")
+end
 end
--- src/.ipynb_checkpoints/Parameters-checkpoint.jl
+++ src/.ipynb_checkpoints/Parameters-checkpoint.jl
@@ -92,17 +92,17 @@ W_l = 19)
 
 
 
-function para_var(va::Dict)
+function para_var(va::NamedTuple)
 	"""
-	Takes in dict with C_1, C_2, ϕ, Γ, η_p, η_m, λ, ψ
-	Returns parameters dict with these values replaced
+	Takes in named tuple with C_1, C_2, ϕ, Γ, η_p, η_m, λ, ψ
+	Returns parameters named tuple with these values replaced
 	"""
 	p_temp_f32 = (σ_1 = 1.0f0,
 	σ_2 = 0.5f0,
 	H_σ_x = 3.0f0,
 	H_σ_y = 0.8f0)
 
-	parameters_f32 = (K = 2,
+	p = (K = 2,
 	δ_v = 1.25f0,
 	δ_c = 0.25f0,
 	δ_m = 0.01875f0,
@@ -177,7 +177,94 @@ function para_var(va::Dict)
 end
 
 
+function para_var_k(va::NamedTuple)
+	"""
+	Takes in named tuple with H_fact, T_fact, T_p_m, W_p_same_fact, W_m_same_fact, H_l, W_l
+	Returns parameters named tuple with these values replaced
+	"""
+	p_temp_f32 = (σ_1 = 1.0f0,
+	σ_2 = 0.5f0,
+	H_σ_x = 3.0f0,
+	H_σ_y = 0.8f0)
 
+	p_temp_f32 = (σ_1 = 1.0f0,
+	σ_2 = 0.5f0,
+	H_σ_x = 3.0f0,
+	H_σ_y = 0.8f0)
+
+	p = (K = 2,
+	δ_v = 1.25f0,
+	δ_c = 0.25f0,
+	δ_m = 0.01875f0,
+	δ_z = 0.125f0,
+	δ_s = 2.5f0,
+	σ_1 = p_temp_f32.σ_1,
+	C_1 = 1.5f0,
+	C_2 = 0.075f0,
+	σ_2 = p_temp_f32.σ_2,
+	γ = 10.0f0,
+	α = 0.5f0,
+	ϕ = 2.0f0,
+	Γ = 0.2f0,
+	v_21 = 0.0f0, #1.0
+	μ = 2.0f0,
+	ν = 1.1f0,
+	n = 6.0f0,
+	att = 0.0f0,# p25
+	η_p = 2.1f0,
+	η_m = 1.5f0,
+	λ = 1.5f0,
+	a_23_ex = 3.0f0,
+	a_23_in = 0.5f0,
+	v12_6 = 1.0f0,
+	v12_4 = 5.0f0,
+	ψ = 0.5f0,
+	C_AB_l =  4*ceil(Int, p_temp_f32.σ_2)+1,
+	H_σ_x = p_temp_f32.H_σ_x,
+	H_σ_y = p_temp_f32.H_σ_y,
+	H_fact = va.H_fact,
+	# H_l =  4*ceil(Int, max(p_temp.H_σ_x, p_temp.H_σ_y))+1,
+	H_l = va.H_l,
+	T_fact = va.T_fac,       #avg TP same orient, other orient
+	T_p_m = va.T_p_m,    #avg TM/TP
+	T_v2_fact = 0.625f0,     #T in V2 = T*
+	# W, H sizes
+	H_size = 19,
+	W_size = 19,
+	# W σ
+	W_p_σ_x_same_a = 6f0,
+	W_p_σ_x_same_b = 3f0, 
+	W_p_σ_y_same_a = 2f0, 	
+	W_p_σ_y_same_b = 4f0, 	
+	W_p_σ_opp_a = 4f0,
+	W_p_σ_x_opp_b = 3f0,
+	W_p_σ_x_opp_c = 1.2f0, 
+	W_p_σ_y_opp_b = 1.2f0, 	
+	W_p_σ_y_opp_c = 3f0, 
+	W_p_same_fact = va.W_p_same_fact,
+	W_p_opp_fact_a = 70f0,
+	W_p_opp_fact_b = 50f0,
+	W_m_σ_x_same_a = 6f0,
+	W_m_σ_x_same_b = 3f0, 
+	W_m_σ_y_same_a = 2.5f0, 	
+	W_m_σ_y_same_b = 4f0, 	
+	W_m_σ_opp_a = 4f0,
+	W_m_σ_x_opp_b = 3f0,
+	W_m_σ_x_opp_c = 1.2f0, 
+	W_m_σ_y_opp_b = 1.2f0, 	
+	W_m_σ_y_opp_c = 3f0,
+	W_m_same_fact = va.W_m_same_fact,
+	W_m_opp_fact_a = 70f0,
+	W_m_opp_fact_b = 50f0,
+	# customn parameters for controling feedback, lgn equlibrum
+	lgn_equ_u = 1.0f0,
+	lgn_equ_A = 0.0f0,
+	lgn_equ_B = 0.0f0,
+	# filling = "circular",
+	# filling = Fill(0f0),
+	W_l = va.W_l)
+	return p
+end
 
 p_temp_f64 = (σ_1 = 1.0,
 σ_2 = 0.5,
