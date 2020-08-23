94723fd333c3625ec3c5a29dece756f1f0077857
Niall Cullinane <niall.cullinane3@mail.dcu.ie>
2020-08-23 16:31:42 +0100

    results

--- scripts/.ipynb_checkpoints/test_illusions-checkpoint.jl
+++ scripts/.ipynb_checkpoints/test_illusions-checkpoint.jl
@@ -1,120 +1,209 @@
 using DrWatson
 @quickactivate "MEngProject"
-using MEngProject, CUDA, DifferentialEquations, PyPlot, NNlib,  ImageFiltering, Images, MEngProject.LaminartKernels, MEngProject.LaminartInitFunc, MEngProject.Utils, BenchmarkTools, Test
+using MEngProject,
+    CUDA,
+    DifferentialEquations,
+    PyPlot,
+    NNlib,
+    ImageFiltering,
+    Images,
+    MEngProject.LaminartKernels,
+    MEngProject.LaminartInitFunc,
+    MEngProject.Utils,
+    BenchmarkTools,
+    Test
 
-using OrdinaryDiffEq, ParameterizedFunctions, LSODA, Sundials, DiffEqDevTools, Noise
+using OrdinaryDiffEq,
+    ParameterizedFunctions, LSODA, Sundials, DiffEqDevTools, Noise
 
 batch = 1
 
 
-files = readdir(datadir("img"))
+# files = readdir(datadir("img"))
 
-@inbounds begin
-	tspan = (0.0f0,800f0)
+files = [
+    	"Iine_gap_1_100_gs.png",
+     "Iine_gap_2_100_gs.png",
+     "Iine_gap_3_100_gs.png",
+     "Iine_gap_4_100_gs.png",
+     "Iines_gaps_100_gs.png",
+     "diag_dots_100_gs.png",
+     "diag_gap_100_gs.png",
+     "kan_sq_cont.png",
+     "kan_sq_cont_l.png",
+     "mo05709.png",
+     "stairs_100gs.png",
+     "stairs_200gs.png",
+    "viper00187.png",
+    "viper00661.jpg",
+    "viper00715.png",
+    "viper00717.png",
+    "viper00720.png",
+    "viper00721.png",
+    "viper00842.jpg",
+    "viper00891.jpg",
+    "viper00904.jpg",
+    "viper00921.jpg",
+    "viper01006.jpg",
+    "viper01333.jpg",
+]
 
-	batch_ = string(batch,"_",rand(1000:9999))
-	mkdir(plotsdir(string("illusions",batch_)))
-	for file in files[2:end]
+batch_ = string(batch, "_", rand(1000:9999))
+mkdir(plotsdir(string("illusions", batch_)))
 
-		p = LaminartInitFunc.parameterInit_conv_gpu(datadir("img",file), Parameters.parameters_f32);
+tspan = (0.0f0, 800f0)
 
-		u0 = cu(reshape(zeros(Float32, p.dim_i, p.dim_j*(5*p.K+2)), p.dim_i, p.dim_j, 5*p.K+2,1))
+for file in files
+    try
+        p = LaminartInitFunc.parameterInit_conv_gpu(
+            datadir("img", file),
+            Parameters.parameters_f32,
+        )
 
-		arr1 = similar(u0[:, :, 1:2,:])
-		arr2 = similar(u0[:, :, 1:1,:])
+        u0 = cu(reshape(
+            zeros(Float32, p.dim_i, p.dim_j * (5 * p.K + 2)),
+            p.dim_i,
+            p.dim_j,
+            5 * p.K + 2,
+            1,
+        ))
 
-		f = LaminartFunc.LamFunction(
-			similar(arr1), #x
-			similar(arr1), #m
-			similar(arr1), #s
-			similar(arr2), #x_lgn,
-			similar(arr1), #C,
-			similar(arr1), #H_z,
-			similar(arr1), # dy_temp,
-			similar(arr1), # dm_temp,
-			similar(arr1), # dz_temp,
-			similar(arr1), # ds_temp,
-			similar(arr2), # dv_temp,
-			similar(arr1), # H_z_temp,
-			similar(arr2), #  V_temp_1,
-			similar(arr2), #  V_temp_2,
-			similar(arr1), #  A_temp,
-			similar(arr1), #   B_temp
-		)
+        arr1 = similar(u0[:, :, 1:2, :])
+        arr2 = similar(u0[:, :, 1:1, :])
 
-		prob = ODEProblem(f, u0, tspan, p)
-	# 	@benchmark sol = solve(prob)
-		sol = solve(prob)
+        f = LaminartFunc.LamFunction(
+            arr1, #x
+            similar(arr1), #m
+            similar(arr1), #s
+            arr2, #x_lgn,
+            similar(arr1), #C,
+            similar(arr1), #H_z,
+            similar(arr1), # dy_temp,
+            similar(arr1), # dm_temp,
+            similar(arr1), # dz_temp,
+            similar(arr1), # ds_temp,
+            similar(arr2), # dv_temp,
+            similar(arr1), # H_z_temp,
+            similar(arr2), #  V_temp_1,
+            similar(arr2), #  V_temp_2,
+            similar(arr1), #  A_temp,
+            similar(arr1), #   B_temp
+        )
 
+        prob = ODEProblem(f, u0, tspan, p)
+        # 	@benchmark sol = solve(prob)
+        sol = solve(prob)
 
-		for t ∈ [25,50,100,200,400,800]
-# 				for t ∈ [25,50,100]
+        # plots
+        for t ∈ [25, 50, 100, 200, 400, 800]
+            # 				for t ∈ [25,50,100]
+            @inbounds begin
 
-			v0 = @view sol(t)[:,:,:,1]
-			axMax = findmax(v0)[1]
+                v0 = @view sol(t)[:, :, :, 1]
+                axMax = findmax(v0)[1]
 
-			for k ∈ 1:2:10
-				fig, ax = plt.subplots()
-				
-				v1 = @view sol(t)[:,:,k,1]
-				v2 = @view sol(t)[:,:,k+1,1]
-				im = ax.imshow(v1, cmap=matplotlib.cm.PRGn,
-							   vmax=axMax, vmin=-axMax)
-				im2 = ax.imshow(v2, cmap=matplotlib.cm.RdBu_r,
-							   vmax=axMax, vmin=-axMax, alpha=0.5)
+                # plot x, y, z, m, s
+                for k ∈ 1:2:10
+                    k2 = k + 1
+                    fig, ax = plt.subplots()
 
-				cbar = fig.colorbar(im2,  shrink=0.9, ax=ax)
-				cbar.ax.set_xlabel("\$k=$k2\$")
-						cbar = fig.colorbar(im,  shrink=0.9, ax=ax)
-				cbar.ax.set_xlabel("\$k=$k\$")
-				layer=Utils.layers[k]
-					plt.title("Layer: $layer, \$t=$t\$")
-					plt.axis("off")
-					fig.tight_layout()
-				plt.savefig(plotsdir(string("illusions",batch_),string(file,"_",t,"_",Utils.la[k],".png")))
-				close("all")
-			end
+                    v1 = @view sol(t)[:, :, k, 1]
+                    v2 = @view sol(t)[:, :, k+1, 1]
+                    im = ax.imshow(
+                        v1,
+                        cmap = matplotlib.cm.PRGn,
+                        vmax = axMax,
+                        vmin = -axMax,
+                    )
+                    im2 = ax.imshow(
+                        v2,
+                        cmap = matplotlib.cm.RdBu_r,
+                        vmax = axMax,
+                        vmin = -axMax,
+                        alpha = 0.5,
+                    )
 
+                    cbar = fig.colorbar(im2, shrink = 0.9, ax = ax)
+                    cbar.ax.set_xlabel("\$k=$k2\$")
+                    cbar = fig.colorbar(im, shrink = 0.9, ax = ax)
+                    cbar.set_alpha(0.5)
+                    cbar.draw_all()
+                    cbar.ax.set_xlabel("\$k=$k\$")
+                    layer = Utils.layers[k]
+                    plt.title("Layer: $layer, \$t=$t\$")
+                    plt.axis("off")
+                    fig.tight_layout()
+                    plt.savefig(plotsdir(
+                        string("illusions", batch_),
+                        string(file, "_", t, "_", Utils.la[k], ".png"),
+                    ))
+                    close("all")
+                end
 
-			k=11
-			fig, ax = plt.subplots()
-			v1 = @view sol[:,:,k,1,t]
-			v2 = @view sol[:,:,k+1,1,t]
-			im = ax.imshow(v1, cmap=matplotlib.cm.PRGn,
-						   vmax=axMax, vmin=-axMax)
-			im2 = ax.imshow(v2, cmap=matplotlib.cm.RdBu_r,
-						   vmax=axMax, vmin=-axMax, alpha=0.5)
+                # plot lgn
+                k = 11
+                fig, ax = plt.subplots()
+                v1 = @view sol(t)[:, :, k, 1]
+                v2 = @view sol(t)[:, :, k+1, 1]
+                im = ax.imshow(
+                    v1,
+                    cmap = matplotlib.cm.PRGn,
+                    vmax = axMax,
+                    vmin = -axMax,
+                )
+                im2 = ax.imshow(
+                    v2,
+                    cmap = matplotlib.cm.RdBu_r,
+                    vmax = axMax,
+                    vmin = -axMax,
+                    alpha = 0.5,
+                )
 
-			cbar = fig.colorbar(im2,  shrink=0.9, ax=ax)
-			cbar.ax.set_xlabel("\$v^-\$")
-					cbar = fig.colorbar(im,  shrink=0.9, ax=ax)
-			cbar.ax.set_xlabel("\$v^+\$")
+                cbar = fig.colorbar(im2, shrink = 0.9, ax = ax)
+                cbar.ax.set_xlabel("\$v^-\$")
+                cbar = fig.colorbar(im, shrink = 0.9, ax = ax)
+                cbar.ax.set_xlabel("\$v^+\$")
+                cbar.set_alpha(0.5)
+                cbar.draw_all()
 
-			layer=Utils.layers[k]
-				plt.title("Layer: $layer, \$t=$t\$")
-				plt.axis("off")
-				fig.tight_layout()
+                layer = Utils.layers[k]
+                plt.title("Layer: $layer, \$t=$t\$")
+                plt.axis("off")
+                fig.tight_layout()
 
-			plt.savefig(plotsdir(string("illusions",batch_),string(file,"_",t,"_",Utils.la[k],".png")))
-			close("all")
-		end
+                plt.savefig(plotsdir(
+                    string("illusions", batch_),
+                    string(file, "_", t, "_", Utils.la[k], ".png"),
+                ))
+                close("all")
+            end
+        end
 
+        # time plot
+        fig, axs = plt.subplots()
 
-	# time plot
-		fig, axs = plt.subplots()
-
-		for k ∈ 1:12
-			v3 = @view sol[:,:,k,1,end]
-			v4 = @view sol[findmax(v3)[2][1],findmax(v3)[2][2],k,1,:]
-			layer=Utils.layers_1[k]
-			axs.plot(v4,Utils.lines[k], label="$layer")
-		end
-		axs.set_xlabel("Time")
-		axs.set_ylabel("Activation")
-		plt.legend()
-		fig.tight_layout()
-		plt.savefig(plotsdir(string("illusions",batch_),string(file,"_time.png")))
-		close("all")
-	end
-	
+        for k ∈ 1:12
+            v3 = @view sol[:, :, k, 1, end]
+            v4 = @view sol[findmax(v3)[2][1], findmax(v3)[2][2], k, 1, :]
+            layer = Utils.layers_1[k]
+            axs.plot(v4, Utils.lines[k], label = "$layer")
+        end
+        axs.set_xlabel("Time")
+        axs.set_ylabel("Activation")
+        plt.legend()
+        fig.tight_layout()
+        plt.savefig(plotsdir(
+            string("illusions", batch_),
+            string(file, "_time.png"),
+        ))
+    finally
+        u0 = nothing
+        p = nothing
+        arr1 = nothing
+        arr2 = nothing
+        f = nothing
+        prob = nothing
+        sol = nothing
+        close("all")
+    end
 end
