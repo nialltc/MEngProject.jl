d0ab62c94bea4626e4808fd5fb5f6daa0863f0c0
Niall <Niall.cullinane3@mail.dcu.ie>
2020-08-24 11:33:06 +0100

    testing run and git

--- scripts/run.jl
+++ scripts/run.jl
@@ -1,62 +1,62 @@
-"""
-# script run
+# """
+# # script run
+#
+# - Julia version: 1.4
+# - Author: niallcullinane
+# - Date: 2020-08-20
+#
+#
+# Runs set of tests and benchmarks.
+# Commits and pushes results to Git.
+# # Examples
+#
+# ```jldoctest
+# julia>
+# ```
+# """
+#
+# using DrWatson
+# @quickactivate "MEngProject"
+# using MEngProject,
+#     CUDA,
+#     DifferentialEquations,
+#     PyPlot,
+#     NNlib,
+#     ImageFiltering,
+#     Images,
+#     MEngProject.LaminartKernels,
+#     MEngProject.LaminartInitFunc,
+#     MEngProject.Utils,
+#     BenchmarkTools,
+#     Test
+#
+# using OrdinaryDiffEq,
+#     ParameterizedFunctions, LSODA, Sundials, DiffEqDevTools, Noise
+#
+# CUDA.allowscalar(false)
+#
+# try include("test_illusions.jl") catch err; print(err) end
+# CUDA.reclaim()
+# try include("test_noise.jl") catch err; print(err) end
+# CUDA.reclaim()
+# try include("test_parameterVar.jl")catch err; print(err) end
+# CUDA.reclaim()
+# try include("test_kernelsVar.jl")catch err; print(err) end
+# CUDA.reclaim()
+# # try include("test_eqAtEquil.jl")catch err; print(err) end
+# CUDA.reclaim()
+#
+#
+# # try include("bench_solver.jl")catch err; print(err) end
+# try include("bench_imp.jl")catch err; print(err) end
+# CUDA.reclaim()
+# # try include("bench_eq.jl")catch err; print(err) end
+# try include("bench_kernels_dim.jl")catch err; print(err) end
+# CUDA.reclaim()
+# try include("bench_dim.jl")catch err; print(err) end
+# CUDA.reclaim()
 
-- Julia version: 1.4
-- Author: niallcullinane
-- Date: 2020-08-20
-
-
-Runs set of tests and benchmarks.
-Commits and pushes results to Git.
-# Examples
-
-```jldoctest
-julia>
-```
-"""
-
-using DrWatson
-@quickactivate "MEngProject"
-using MEngProject,
-    CUDA,
-    DifferentialEquations,
-    PyPlot,
-    NNlib,
-    ImageFiltering,
-    Images,
-    MEngProject.LaminartKernels,
-    MEngProject.LaminartInitFunc,
-    MEngProject.Utils,
-    BenchmarkTools,
-    Test
-
-using OrdinaryDiffEq,
-    ParameterizedFunctions, LSODA, Sundials, DiffEqDevTools, Noise
-
-CUDA.allowscalar(false)
-
-try include("test_illusions.jl") catch err; print(err) end
-CUDA.reclaim()
-try include("test_noise.jl") catch err; print(err) end
-CUDA.reclaim()
-try include("test_parameterVar.jl")catch err; print(err) end
-CUDA.reclaim()
-try include("test_kernelsVar.jl")catch err; print(err) end
-CUDA.reclaim()
-# try include("test_eqAtEquil.jl")catch err; print(err) end
-CUDA.reclaim()
-
-
-# try include("bench_solver.jl")catch err; print(err) end
-try include("bench_imp.jl")catch err; print(err) end
-CUDA.reclaim()
-# try include("bench_eq.jl")catch err; print(err) end
-try include("bench_kernels_dim.jl")catch err; print(err) end
-CUDA.reclaim()
-try include("bench_dim.jl")catch err; print(err) end
-CUDA.reclaim()
-
-run(`git add plots/*`)
-run(`git commit -m "results"`)
 run(`git pull`)
+run(`git add plots/*`)
+run(`git commit -m "test"`)
 run(`git push`)
