2cc3ff91c7e90d41818b3593994864d8bbd2cefe
Niall <Niall.cullinane3@mail.dcu.ie>
2020-08-24 11:35:26 +0100

    typos

--- scripts/run.jl
+++ scripts/run.jl
@@ -15,46 +15,46 @@
 # ```
 # """
 #
-# using DrWatson
-# @quickactivate "MEngProject"
-# using MEngProject,
-#     CUDA,
-#     DifferentialEquations,
-#     PyPlot,
-#     NNlib,
-#     ImageFiltering,
-#     Images,
-#     MEngProject.LaminartKernels,
-#     MEngProject.LaminartInitFunc,
-#     MEngProject.Utils,
-#     BenchmarkTools,
-#     Test
-#
-# using OrdinaryDiffEq,
-#     ParameterizedFunctions, LSODA, Sundials, DiffEqDevTools, Noise
-#
-# CUDA.allowscalar(false)
-#
-# try include("test_illusions.jl") catch err; print(err) end
-# CUDA.reclaim()
-# try include("test_noise.jl") catch err; print(err) end
-# CUDA.reclaim()
-# try include("test_parameterVar.jl")catch err; print(err) end
-# CUDA.reclaim()
-# try include("test_kernelsVar.jl")catch err; print(err) end
-# CUDA.reclaim()
-# # try include("test_eqAtEquil.jl")catch err; print(err) end
-# CUDA.reclaim()
-#
-#
-# # try include("bench_solver.jl")catch err; print(err) end
-# try include("bench_imp.jl")catch err; print(err) end
-# CUDA.reclaim()
-# # try include("bench_eq.jl")catch err; print(err) end
-# try include("bench_kernels_dim.jl")catch err; print(err) end
-# CUDA.reclaim()
-# try include("bench_dim.jl")catch err; print(err) end
-# CUDA.reclaim()
+using DrWatson
+@quickactivate "MEngProject"
+using MEngProject,
+    CUDA,
+    DifferentialEquations,
+    PyPlot,
+    NNlib,
+    ImageFiltering,
+    Images,
+    MEngProject.LaminartKernels,
+    MEngProject.LaminartInitFunc,
+    MEngProject.Utils,
+    BenchmarkTools,
+    Test
+
+using OrdinaryDiffEq,
+    ParameterizedFunctions, LSODA, Sundials, DiffEqDevTools, Noise
+
+CUDA.allowscalar(false)
+
+try include("test_illusions.jl") catch err; print(err) end
+CUDA.reclaim()
+try include("test_noise.jl") catch err; print(err) end
+CUDA.reclaim()
+try include("test_parameterVar.jl") catch err; print(err) end
+CUDA.reclaim()
+try include("test_kernelsVar.jl") catch err; print(err) end
+CUDA.reclaim()
+# try include("test_eqAtEquil.jl")catch err; print(err) end
+CUDA.reclaim()
+
+
+# try include("bench_solver.jl")catch err; print(err) end
+try include("bench_imp.jl") catch err; print(err) end
+CUDA.reclaim()
+# try include("bench_eq.jl")catch err; print(err) end
+try include("bench_kernels_dim.jl") catch err; print(err) end
+CUDA.reclaim()
+try include("bench_dim.jl") catch err; print(err) end
+CUDA.reclaim()
 
 run(`git pull`)
 run(`git add plots/*`)
