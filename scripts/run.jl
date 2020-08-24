0ab6d41686fb17d483879b9342b07ef248147771
Niall <Niall.cullinane3@mail.dcu.ie>
2020-08-24 13:11:18 +0100

    script update

--- scripts/run.jl
+++ scripts/run.jl
@@ -36,27 +36,27 @@ using OrdinaryDiffEq,
 CUDA.allowscalar(false)
 
 try include("test_illusions.jl") catch err; print(err) end
-CUDA.reclaim()
-try include("test_noise.jl") catch err; print(err) end
-CUDA.reclaim()
-try include("test_parameterVar.jl") catch err; print(err) end
-CUDA.reclaim()
-try include("test_kernelsVar.jl") catch err; print(err) end
-CUDA.reclaim()
-# try include("test_eqAtEquil.jl")catch err; print(err) end
-CUDA.reclaim()
-
-
-# try include("bench_solver.jl")catch err; print(err) end
-try include("bench_imp.jl") catch err; print(err) end
-CUDA.reclaim()
-# try include("bench_eq.jl")catch err; print(err) end
-try include("bench_kernels_dim.jl") catch err; print(err) end
-CUDA.reclaim()
-try include("bench_dim.jl") catch err; print(err) end
-CUDA.reclaim()
-
-run(`git pull`)
-run(`git add plots/*`)
-run(`git commit -m "test"`)
-run(`git push`)
+# CUDA.reclaim()
+# try include("test_noise.jl") catch err; print(err) end
+# CUDA.reclaim()
+# try include("test_parameterVar.jl") catch err; print(err) end
+# CUDA.reclaim()
+# try include("test_kernelsVar.jl") catch err; print(err) end
+# CUDA.reclaim()
+# # try include("test_eqAtEquil.jl")catch err; print(err) end
+# CUDA.reclaim()
+#
+#
+# # try include("bench_solver.jl")catch err; print(err) end
+# try include("bench_imp.jl") catch err; print(err) end
+# CUDA.reclaim()
+# # try include("bench_eq.jl")catch err; print(err) end
+# try include("bench_kernels_dim.jl") catch err; print(err) end
+# CUDA.reclaim()
+# try include("bench_dim.jl") catch err; print(err) end
+# CUDA.reclaim()
+#
+# run(`git pull`)
+# run(`git add plots/*`)
+# run(`git commit -m "test"`)
+# run(`git push`)
